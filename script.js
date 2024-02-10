a0189b466802700995e78d5bf4d3a96bb127cd25
p6tgrph <159617360+p6tgrph@users.noreply.github.com>
2024-02-10 14:06:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openALL() {
+    window.location.href = 'index.html';
+}
+
+function openWEEKSPECIAL() {
+    window.location.href = 'weekspecial.html';
+}
+
+function openOTHER() {
+    window.location.href = 'other.html';
+}
